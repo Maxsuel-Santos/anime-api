f5e9c98f9b1dfbbdffd0a5203b0228efa1fc3502
Maxsuel Santos <maxsuel.ap.l.santos@gmail.com>
2025-12-31 17:58:49 -0300

    Docker & Domain

--- src/main/java/com/github/maxsuel/anime_api/domain/Anime.java
+++ src/main/java/com/github/maxsuel/anime_api/domain/Anime.java
@@ -1,20 +1,27 @@
 package com.github.maxsuel.anime_api.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
 @Table(name = "animes")
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Anime {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
 
+    @Column(nullable = false)
+    private String name;
 }
--- src/main/java/com/github/maxsuel/anime_api/repository/AnimeRepository.java
+++ src/main/java/com/github/maxsuel/anime_api/repository/AnimeRepository.java
@@ -1,5 +1,10 @@
 package com.github.maxsuel.anime_api.repository;
 
-public class AnimeRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.github.maxsuel.anime_api.domain.Anime;
+
+@Repository
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
 }
