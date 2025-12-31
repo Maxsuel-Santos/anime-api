8abb0e168254772a7329db07dc540ef87def03be
Maxsuel Santos <maxsuel.ap.l.santos@gmail.com>
2025-12-31 17:03:30 -0300

    Docker configurations and JPA and Swagger dependencies

--- src/main/java/com/github/maxsuel/anime_api/domain/Anime.java
+++ src/main/java/com/github/maxsuel/anime_api/domain/Anime.java
@@ -1,12 +1,19 @@
 package com.github.maxsuel.anime_api.domain;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@Table(name = "animes")
 @Data
 @AllArgsConstructor
 public class Anime {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
