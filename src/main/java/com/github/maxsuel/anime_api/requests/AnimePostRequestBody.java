a56665b97c966ac55d13f28326e5ee6fd8ab20ea
Maxsuel Santos <maxsuel.ap.l.santos@gmail.com>
2026-01-07 00:00:34 -0300

    Handler para validação de campos

--- src/main/java/com/github/maxsuel/anime_api/requests/AnimePostRequestBody.java
+++ src/main/java/com/github/maxsuel/anime_api/requests/AnimePostRequestBody.java
@@ -1,5 +1,6 @@
 package com.github.maxsuel.anime_api.requests;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -9,6 +10,7 @@ public class AnimePostRequestBody {
  
     @NotEmpty(message = "The anime name cannot be empty")
     @NotNull(message = "The anime name cannot be null")
+    @NotBlank(message = "The anime name cannot be empty")
     private String name;
 
 }
